3e3b7027bc9ab3aa4504c51254b694197aa5f447
Emil Petersson <emilpet93@gmail.com>
2020-05-02 17:32:39 +0200

    Established connection to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const oauth = require('./routes/oauth')
 
+const app = express()
 app.route('/oauth').get(oauth.get)
+
+mongoose.set('useCreateIndex', true)
+const db = `mongodb+srv://admin-emil:${process.env.PASSWORD}@userdata-43fxt.mongodb.net/test?retryWrites=true&w=majority`
+mongoose
+    .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB!')
+    })
+    .catch(({ stack }) => {
+        throw new Error(stack)
+    })
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
